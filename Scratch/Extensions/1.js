ac16a03638b475e08d4a3c1603f3bd42bc960280
Dawn <142942001+Dawnthedemon@users.noreply.github.com>
2024-10-21 19:01:28 -0400

    Create 1.js

---
+++ Scratch/Extensions/1.js
@@ -0,0 +1,11 @@
+async function startCapture(displayMediaOptions) {
+  let captureStream = null;
+
+  try {
+    captureStream =
+      await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+  } catch (err) {
+    console.error(`Error: ${err}`);
+  }
+  return captureStream;
+}
